3a2805334f422ae8a3b1296bc4a175425ac3f989
Michael Roose <mjroose@gmail.com>
2018-11-26 16:24:30 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
-      if artist = Artist.find(params[:artist_id]) && !(@song = Song.find(params[:id]))
+      if artist = Artist.find(params[:artist_id]) && !(@song = Song.find_by(id: params[:id]))
         redirect_to artist_path(artist)
       end
     end
